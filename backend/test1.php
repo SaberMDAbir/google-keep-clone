18c1ebc6a365061d9107acb7df1cedfcdd19d9b5
slee1717 <samx17@gmail.com>
2020-05-14 20:23:07 -0400

    implemented header and footer

--- backend/test1.php
+++ backend/test1.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-    crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="script.js"></script>
-    <link rel="icon" href="google-keep.png" type="image/ico">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">  </head>
+<?php
+include('header.php');
+?>
 <body>
 
 
@@ -118,6 +108,7 @@ elseif (!empty($_POST['newEmail'])) {
        ?>
     </div>
 </div>
+<?php
+include('footer.php');
+?>
 
-</body>
-</html>
